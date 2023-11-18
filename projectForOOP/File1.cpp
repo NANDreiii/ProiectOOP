40777265140d64b09a77ebeb2b436e97ae403cca
Andrei Nitu <andreiraul.nitu@gmail.com>
2023-11-18 03:25:03 +0200

    Create first 2 classes: Ticket & event

--- projectForOOP/File1.cpp
+++ projectForOOP/File1.cpp
@@ -1,5 +1,20 @@
 #include<iostream>
 using namespace std;
+class Ticket {
+	int uniqueId;
+	double priceOfTicket;
+	int row;
+	int seat;
+	char* zone;
+	
+
+
+};
+class event {
+	const char* nameOfEvent;
+	int numberOfSeats;
+	
+};
 int main()
 {
 	cout << "De testare";
