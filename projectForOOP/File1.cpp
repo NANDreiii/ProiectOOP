d23d1ae9a5feed19a6903b504ce9f6f73be30260
Andrei Nitu <andreiraul.nitu@gmail.com>
2023-11-25 02:13:06 +0200

    Add enum class Add static variable for class Ticket

--- projectForOOP/File1.cpp
+++ projectForOOP/File1.cpp
@@ -5,30 +5,46 @@ private:
 	const int uniqueId;
 	int row;
 	int seat;
+	int price;//added
 	char* zone;
 public:
 	static int ticketsSold;
 	Ticket() :uniqueId(0) {//default constr
 		this->row = 0;
 		this->seat = 0;
-		this->zone = "VIP";
+		this->price = 0;
+		this->zone = NULL;
 	}
 	//constr with parameters
-	Ticket(int row, int seat, char* zone) :uniqueId(++ticketsSold) {
+	Ticket(int row, int seat, int price, char* zone) :uniqueId(++ticketsSold) {
 		this->row = row;
 		this->seat = seat;
+		this->price = price;
 		this->zone = new char[strlen(zone) + 1];
 		strcpy(this->zone, zone);
 	}
 
 
 };
+class Location {
+	int maxNumberSeats;
+	int numberOfRows;
+	int zones;
+
+};
 class Event {
 private:
-	const char* nameOfEvent;
+	const int eventId;//added
+	char* nameOfEvent;
 	int numberOfSeats;
 [public:
-	static int 
+	//default constr
+	Event() :eventId(0) {
+		this->nameOfEvent = NULL;
+		this->numberOfSeats = 0;
+	}
+	
+
 	
 };
 int main()
